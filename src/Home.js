bfc5896569bf9348814bd65e9aff52583157da2f
Taewoong Moon <moondingie5@gmail.com>
2019-03-11 00:46:38 +0900

    graphql 클라인트에서 받기

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Query } from "react-apollo";
+import { HOME_PAGE } from './queries'
 
-const Home = () => "Home"
+// const Home = () => <Query query={HOME_PAGE}></Query>
+
+const Home = () => (
+    <Query query={HOME_PAGE}>
+        {({ loading, data, error }) => {
+            if (loading) return <span>loading</span>
+            if (error) return <span>something happend</span>
+            if (data) {
+                console.log('data..', data)
+                return <span>yay!!</span>
+            }
+        }}
+
+    </Query>
+)
 
 export default Home;
