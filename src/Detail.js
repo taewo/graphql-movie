871819d133033e82ce786d62bf692048df9591c6
Taewoong Moon <moondingie5@gmail.com>
2019-03-12 11:06:55 +0900

    코드 리셋

--- src/Detail.js
+++ src/Detail.js
@@ -1,21 +1,5 @@
 import React from 'react';
-import { Query } from 'react-apollo';
-import { MOVIE_DETAILS } from './queries';
 
-const Detail = ({ match }) => (
-    <Query query={MOVIE_DETAILS}>
+const Detail = () => "Detail"
 
-        {({ loading, data, error }) => {
-            console.log(1, match)
-            if (loading) return <span>loading</span>
-            if (error) return <span>error</span>
-            return (
-                <React.Fragment>
-                    Detail
-                </React.Fragment>
-            )
-        }}
-    </Query>
-)
-
-export default Detail;
+export defualt Detail;
--- src/apolloClient.js
+++
@@ -1,7 +0,0 @@
-import ApolloClient from "apollo-boost";
-
-const client = new ApolloClient({
-	uri: "https://movieql.now.sh/"
-})
-
-export default client;
