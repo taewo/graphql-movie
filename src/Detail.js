7a6f7da614bb4d630b7d2643f58bdfa5d222e15f
Taewoong Moon <moondingie5@gmail.com>
2019-03-13 15:37:04 +0900

    코드 수정

--- src/Detail.js
+++ src/Detail.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import { MOVIE_DETAILS } from './queries';
 
-const Detail = () => {
+const Detail = ({match}) => {
+    let { movieId } = match.params;
     return (
-        <Query query={MOVIE_DETAILS}>
+        <Query
+            query={MOVIE_DETAILS}
+            variables={{movieId}}
+        >
             {({ loading, error, data}) => {
                 if (loading) return <span>loading</span>
                 if (error) return <span>error</span>
-                return <span>data</span>
+                console.log('data', data)
+                return (
+                <span>
+                    123
+                </span>
+                )
             }}
         </Query>
     )
